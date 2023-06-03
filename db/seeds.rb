dc42785e274c4c8c98497b2624c4ba23e9d13cb8
Alexandre Stanescot <alex@aerostan.com>
2023-06-03 19:38:02 +0200

    Seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# ############################################
+# CLEANING
+# ############################################
+puts "Cleaning databases..."
+User.destroy_all
+
+# ############################################
+# USERS
+# ############################################
+# Deleting current users
+
+# Create an admin user
+user = User.new
+user.first_name = "Alexandre"
+user.last_name = "Stanescot"
+user.email = "alex@aerostan.com"
+user.role = "admin"
+user.password = "alex@aerostan.com"
+user.confirmed_at = Time.zone.now - 1.hour
+user.confirmation_sent_at = Time.zone.now - 2.hours
+user.save
+puts "Admin user alex created"
+
+# Create an user
+if Rails.env.development?
+  user = User.new
+  user.first_name = "Rachel"
+  user.last_name = "Muller"
+  user.email = "rachel.fly@me.com"
+  user.role = "user"
+  user.password = "rachel.fly@me.com"
+  user.confirmed_at = Time.zone.now - 1.hour
+  user.confirmation_sent_at = Time.zone.now - 2.hours
+  user.save
+  puts "User rachel created in dev"
+end
+
+puts "Seeds complete! 🌻"
