ce45ad63ae359c24f9fab0c428a459298a2995e7
Alexandre Stanescot <alex@aerostan.com>
2023-06-15 20:42:43 +0200

    airport search feature

--- app/controllers/trip_requests_controller.rb
+++ app/controllers/trip_requests_controller.rb
@@ -1,7 +1,13 @@
+require 'uri'
+
 class TripRequestsController < ApplicationController
+  before_action :set_base_url, only: [:new, :create, :edit, :update]
+
   def new
     @trip_request = TripRequest.new
     @trip_request.user = current_user
+    @trip_request.airport_id = params[:airport].to_i
+    set_airport_details if @trip_request.airport_id.positive?
   end
 
   def create
@@ -12,4 +18,20 @@ class TripRequestsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def set_base_url
+    url = request.url
+    uri = URI.parse(url)
+    @base_url = "#{uri.scheme}://#{uri.host}:#{uri.port}"
+  end
+
+  def set_airport_details
+    airport = Airport.find(@trip_request.airport_id)
+    airport_name = airport.name
+    airport_icao = airport.icao
+    @airport_full_name = "#{airport_name} (#{airport_icao})"
+  end
+
 end
--- app/javascript/controllers/airport_searcher_controller.js
+++ app/javascript/controllers/airport_searcher_controller.js
@@ -6,21 +6,19 @@ export default class extends Controller {
 
   connect() {
     console.log("Airport searcher connected!");
+    this.baseUrl = this.data.get('base-urlValue');
   }
 
   display = (array) => {
-    console.log(array);
     this.resultTarget.innerHTML = "";
     array.forEach((airport) => {
-      const line = `<li id="${airport.id}">${airport.name} (${airport.icao})</li>`;
+      const line = `<li><a id="${airport.id}" href="${this.baseUrl}/trip_requests/new?airport=${airport.id}">${airport.name} (${airport.icao})</a></li>`;
       this.resultTarget.insertAdjacentHTML("afterbegin", line);
     });
   };
 
   eventKey(e) {
-    //TODO: Retrieve the api url from correct environment
-    const url = "http://localhost:3000/api/v1/airports/find?query=";
-    console.log(`${url}${this.searchboxTarget.value}`);
+    const url = `${this.baseUrl}/api/v1/airports/find?query=`;
     fetch(`${url}${this.searchboxTarget.value}`)
       .then(response => response.json())
       .then(data => this.display(data));
