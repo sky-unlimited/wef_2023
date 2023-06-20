ccb281b66ec61570fc1f9fad2352e2826d640ac8
Alexandre Stanescot <alex@aerostan.com>
2023-06-20 18:07:21 +0200

    created service forlder and weather service

--- app/controllers/trip_suggestions_controller.rb
+++ app/controllers/trip_suggestions_controller.rb
@@ -1,5 +1,14 @@
 class TripSuggestionsController < ApplicationController
   def index
+    # We load the current user last trip request
     @trip_request = TripRequest.where(user_id: current_user.id).order(id: :desc).first
+
+    # We check the weather at departure airport both for start date and return date (if provided)
+      # If one of the checked dates is below user's acceptance threshold we display a message and next days weather
+      # We exit the process
+    # We define the elastic weather for departure date
+    # We define the elastic weather for return date
+    # We combine the 2 in a fly zone ST_Intersection -> https://chat.openai.com/share/c725d3ab-16f3-42f1-8ca6-972038393e93
+    # We display it on a Leaflet map.
   end
 end
---
+++ app/services/weather_service.rb
@@ -0,0 +1,13 @@
+class WeatherService
+  def initialize
+  end
+
+  def getWeather(lon, lat)
+  end
+
+  private
+
+  def airport_to_tile_center(lon, lat)
+  end
+
+end
