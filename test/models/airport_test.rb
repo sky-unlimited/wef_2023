fa1925ef9bd7f244a278e60d36fa20735a677735
Alexandre Stanescot <alex@aerostan.com>
2023-07-11 18:28:42 +0200

    closes#44

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -7,17 +7,19 @@ class AirportTest < ActiveSupport::TestCase
 
     test "should not create a military airport" do
       airport_hash = { icao: "LFKS", name: "Solenzara (BA 126) Air Base", city: "Solenzara", country: countries(:france), latitude: 41.924400329589844, longitude: 9.406000137329102, altitude: 28, airport_type: "medium_airport" }
-      assert_not Airport.create(airport_hash).save
+      record = Airport.new(airport_hash)
+      assert_not record.save
     end
 
     test "should not create a heliport" do
       airport_hash = { icao: "LFPI", name: "Paris Issy-les-Moulineaux", city: "Paris", country: countries(:france), latitude: 48.833302, longitude: 2.27278, altitude: 112, airport_type: "heliport" }
-      assert_not Airport.create(airport_hash).save
+      record = Airport.new(airport_hash)
+      assert_not record.save
     end
-
 
     test "should create a white listed airport" do
       airport_hash = { icao: "LFJY", name: "Chambley Air Base", city: "Chambley", country: countries(:france), latitude: 49.025501, longitude: 5.87607, altitude: 866, airport_type: "small_airport" }
-      assert Airport.create(airport_hash)
+      record = Airport.new(airport_hash)
+      assert record.save
     end
 end
