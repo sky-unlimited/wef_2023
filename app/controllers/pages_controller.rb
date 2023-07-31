226d2387aba17272935a6d953f999456692e5775
Alexandre Stanescot <alex@aerostan.com>
2023-07-31 18:17:45 +0200

    closes#65

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,43 @@ class PagesController < ApplicationController
     distinct_countries.each do |country|
       @countries_array.push country.code
     end
+
+    # We count the number of amenities available in our database
+    @accomodation_count = 0
+    amenities = ['hostel', 'hotel', 'chalet', 'motel', 'guest_house']
+    @accomodation_count += OsmPoint.where(amenity: amenities).count
+    @accomodation_count += OsmLine.where(amenity: amenities).count
+    @accomodation_count += OsmPolygone.where(amenity: amenities).count
+
+    @restaurants_count = 0
+    amenities = ['restaurant', 'fast-food']
+    @restaurants_count += OsmPoint.where(amenity: amenities).count
+    @restaurants_count += OsmLine.where(amenity: amenities).count
+    @restaurants_count += OsmPolygone.where(amenity: amenities).count
+
+    @bus_stations_count = 0
+    amenities = ['bus_station', 'bus_stop']
+    @bus_stations_count += OsmPoint.where(amenity: amenities).count
+    @bus_stations_count += OsmLine.where(amenity: amenities).count
+    @bus_stations_count += OsmPolygone.where(amenity: amenities).count
+
+    @bicycle_rental_count = 0
+    amenities = ['bicycle_rental']
+    @bicycle_rental_count += OsmPoint.where(amenity: amenities).count
+    @bicycle_rental_count += OsmLine.where(amenity: amenities).count
+    @bicycle_rental_count += OsmPolygone.where(amenity: amenities).count
+
+    @camp_sites_count = 0
+    amenities = ['camp_site']
+    @camp_sites_count += OsmPoint.where(amenity: amenities).count
+    @camp_sites_count += OsmLine.where(amenity: amenities).count
+    @camp_sites_count += OsmPolygone.where(amenity: amenities).count
+
+    @bars_count = 0
+    amenities = ['bar', 'cafe']
+    @bars_count += OsmPoint.where(amenity: amenities).count
+    @bars_count += OsmLine.where(amenity: amenities).count
+    @bars_count += OsmPolygone.where(amenity: amenities).count
   end
 
   def console
