6ab5bb7c3bf4433f0dca5fae5c8ef5fa5fe3c3c3
Alexandre Stanescot <alex@aerostan.com>
2023-08-18 13:29:14 +0200

    Test emails should have valid domains

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,21 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  # provided emails should have valid domain names
   test "Correct user should save" do
     user = users(:regular_user)
     assert user.save
   end
 
   test "user missing last_name should not save" do
-    assert_not User.new(email: "test@example.com", password: "123456", username: "josmith11", first_name: "First name").save
+    assert_not User.new(email: "test@as-consult.io", password: "123456", username: "josmith11", first_name: "First name").save
   end
 
   test "user missing first_name should not save" do
-    assert_not User.new(email: "test@example.com", password: "123456", username: "username1", last_name: "last name").save
+    assert_not User.new(email: "test@as-consult.io", password: "123456", username: "username1", last_name: "last name").save
   end
 
   test "new created user should have a role" do
-    user = User.new(email: "test@example.com", password: "123456", username: "username2", last_name: "last name", first_name: "first name")
+    user = User.new(email: "test@as-consult.io", password: "123456", username: "username2", last_name: "last name", first_name: "first name")
     user.save!
     assert_equal user.role.nil?, false
   end
@@ -30,6 +31,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "already taken username should not save" do
-    assert_not User.new(email: "test2@example.com", password: "123456", username: "josmith66", first_name: "First name").save
+    assert_not User.new(email: "test2@as-consult.io", password: "123456", username: "josmith66", first_name: "First name").save
   end
 end
