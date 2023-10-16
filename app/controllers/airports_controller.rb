504ce47371305866ff94107d0f728cc47532612d
Alexandre Stanescot <alex@aerostan.com>
2023-10-16 11:56:47 +0200

    VAC link

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -8,5 +8,12 @@ class AirportsController < ApplicationController
   def show
     @airport = Airport.find(params[:id])
     @runways = @airport.runways
+
+    # Link to Visual Airport Chart if available
+    # France
+    if @airport.icao.match?(/^LF..$/) #VAC only for ICAO airports
+      aip_cycle = WEF_CONFIG['sia_current_cycle'].freeze
+      @vac_link = "https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_#{aip_cycle}/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.#{@airport.icao}.pdf"
+    end
   end
 end
