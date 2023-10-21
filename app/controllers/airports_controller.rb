03454fabdbf46bcc4cc26dc69a76545bbde32288
Alexandre Stanescot <alex@aerostan.com>
2023-10-21 12:54:27 +0200

    osm_polygones converted to centroid

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,5 +1,6 @@
 require 'rgeo'
 require 'rgeo/proj4'
+require 'rgeo-geojson'
 
 class AirportsController < ApplicationController
   # GET /airports
@@ -19,17 +20,36 @@ class AirportsController < ApplicationController
       @vac_link = "https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_#{aip_cycle}/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.#{@airport.icao}.pdf"
     end
 
-    # Load the oms_points
+    # Load the osm_points
     @points_array = []
     osm_points = OsmPoint.where(airport_id: @airport.id)
     osm_points.each do |osm_point|
       # Convert projection to geography
-      geography = RGeo::CoordSys::Proj4.create(4326) #geography
-      projection = RGeo::CoordSys::Proj4.create(3857) #projection
+      geography   = RGeo::CoordSys::Proj4.create(4326)
+      projection  = RGeo::CoordSys::Proj4.create(3857)
       x,y = RGeo::CoordSys::Proj4.transform_coords(projection, geography, osm_point.way.x, osm_point.way.y, nil)
 
       point =  {  :id => osm_point.id,
                   :name => osm_point.osm_name,
+                  :latitude => y,
+                  :longitude => x }
+      @points_array << point
+    end
+
+    # Load osm_polygons but represented as points based on centroid
+    @polygones_array = []
+    osm_polygones = OsmPolygone.where(airport_id: @airport.id)
+    osm_polygones.each do |osm_polygone|
+      # We first deduct the centroid of thr polygone in order to display it as a marker
+      polygone_centroid = osm_polygone.way.centroid
+
+      # Convert projection to geography
+      geography   = RGeo::CoordSys::Proj4.create(4326)
+      projection  = RGeo::CoordSys::Proj4.create(3857)
+      x,y = RGeo::CoordSys::Proj4.transform_coords(projection, geography, polygone_centroid.x, polygone_centroid.y, nil)
+
+      point =  {  :id => osm_polygone.id,
+                  :name => osm_polygone.osm_name,
                   :latitude => y,
                   :longitude => x }
       @points_array << point
