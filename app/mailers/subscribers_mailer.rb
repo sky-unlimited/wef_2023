d83c9372d4e8583046b42ff6180f0293ad5e0965
Alexandre Stanescot <alex@aerostan.com>
2023-11-10 20:14:19 +0100

    Apply default layout to subscribers mail

--- app/mailers/subscribers_mailer.rb
+++ app/mailers/subscribers_mailer.rb
@@ -1,6 +1,9 @@
 class SubscribersMailer < ApplicationMailer
 
   def subscribed
+    # Attach image
+    attachments.inline["full-logo-early-dark.png"] = File.read("#{Rails.root}/app/assets/images/full-logo-early-dark.png")
+
     @subscriber = params[:subscriber]
     @link_unsubscribe = params[:link]
     @link_root = root_url
