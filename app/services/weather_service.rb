70e6827ebfb6c9837c260cc658a1c5876b15e12c
Alexandre Stanescot <alex@aerostan.com>
2023-11-22 13:42:42 +0100

    Refactoring init - fake weather

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,6 +1,29 @@
 require 'rest-client'
 
 class WeatherService
+
+  @@weather_conditions = [
+    { "id" => 210, "main" => "Thunderstorm", "description" => I18n.t('weather.210'), "icon" => "11d" },
+    { "id" => 212, "main" => "Thunderstorm", "description" => I18n.t('weather.212'), "icon" => "11d" },
+    { "id" => 300, "main" => "Drizzle", "description" => I18n.t('weather.300'), "icon" => "09d" },
+    { "id" => 310, "main" => "Drizzle", "description" => I18n.t('weather.310'), "icon" => "09d" },
+    { "id" => 500, "main" => "Rain", "description" => I18n.t('weather.500'), "icon" => "10d" },
+    { "id" => 501, "main" => "Rain", "description" => I18n.t('weather.501'), "icon" => "10d" },
+    { "id" => 503, "main" => "Rain", "description" => I18n.t('weather.503'), "icon" => "10d" },
+    { "id" => 520, "main" => "Rain", "description" => I18n.t('weather.520'), "icon" => "09d" },
+    { "id" => 600, "main" => "Snow", "description" => I18n.t('weather.600'), "icon" => "13d" },
+    { "id" => 602, "main" => "Snow", "description" => I18n.t('weather.602'), "icon" => "13d" },
+    { "id" => 800, "main" => "Clear", "description" => I18n.t('weather.800'), "icon" => "01d" },
+    { "id" => 801, "main" => "Clouds", "description" => I18n.t('weather.801'), "icon" => "02d" },
+    { "id" => 802, "main" => "Clouds", "description" => I18n.t('weather.802'), "icon" => "03d" },
+    { "id" => 803, "main" => "Clouds", "description" => I18n.t('weather.803'), "icon" => "04d" },
+    { "id" => 804, "main" => "Clouds", "description" => I18n.t('weather.804'), "icon" => "04d" }
+  ]
+
+  def self.weather_conditions
+   @@weather_conditions
+  end 
+
   def self.get_weather(lat, lon)
     # We check if weather available in database and inside validity
     db_weather = WeatherCall.where(lon: lon, lat: lat).last
@@ -16,23 +39,33 @@ class WeatherService
       end
     end
 
-    # If not, we call the weather api
-    api_call = RestClient.get 'https://api.openweathermap.org/data/3.0/onecall', 
-                              {params: 
-                                       {lat: lat, 
-                                        lon: lon, 
-                                        appid: ENV["OPENWEATHERMAP_API"],
-                                        exclude: "current,hourly,minutely",
-                                        units: "metric"}
-                              }
-    weather_call = JSON.parse( api_call )
+    # If no data present in database, 2 scenarios:
+    # 1. If we provide real weather, we call the weather API
+    # 2. If we don't provide real weather, we generate fake weather in database
+    if WEF_CONFIG['fake_weather'] == false
+      # If not, we call the weather api
+      api_call = RestClient.get 'https://api.openweathermap.org/data/3.0/onecall', 
+                                {params: 
+                                         {lat: lat, 
+                                          lon: lon, 
+                                          appid: ENV["OPENWEATHERMAP_API"],
+                                          exclude: "current,hourly,minutely",
+                                          units: "metric"}
+                                }
+      weather_call = JSON.parse( api_call )
 
-    # We store it in database
-    WeatherCall.create(lat: weather_call["lat"].round(6),
-                       lon: weather_call["lon"].round(6),
-                       json: api_call)
+      # We store it in database
+      WeatherCall.create(lat: weather_call["lat"].round(6),
+                         lon: weather_call["lon"].round(6),
+                         json: api_call)
 
-    # And we retrive the id of the record in database
+    else
+      # We generate fake weather
+
+    end
+
+
+    # And we retrive the id of the record in database real or fake
     return  WeatherCall.where(  lat: weather_call["lat"].round(6),
                                 lon: weather_call["lon"].round(6)
                              ).pluck(:id).last
@@ -158,6 +191,30 @@ class WeatherService
     else
       find_weather_description_by_id(503)
     end
+  end
+
+  def self.create_fake_weather(lon, lat)
+    # We gather all available weather codes
+    weather_codes_all = @@weather_conditions.map { |weather_condition| weather_condition["id"] }
+    
+    # We pick a random weather
+    random_weather_id = weather_codes_all.sample
+
+    # We build a json following OpenWeather API 3.0 specs
+    weather_description_hash = @@weather_conditions.find { |weather_condition| weather_condition["id"] == random_weather_id }
+
+    # TODO: The hash should be looped 7 times with different weather conditions for each day!
+    hash = {  "lat" => lat,
+              "lon" => lon,
+              "daily" => [
+                {
+                  "wind_speed"  => rand(5..30),
+                  "wind_deg"    => rand(0..360)
+                }
+              ] }
+
+    return hash
+
   end
 
 end
