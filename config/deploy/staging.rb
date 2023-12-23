b777f5b22704c4eb31fe1f4638b0322955f192c7
Alexandre Stanescot <alex@aerostan.com>
2023-12-23 10:56:48 +0100

    Deploy staging updates

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '192.168.77.19', user: 'deploy', roles: %w{app db web}
+server 'mac-001', user: 'deploy', roles: %w{app db web}, port: 8922
 
 
 # role-based syntax
