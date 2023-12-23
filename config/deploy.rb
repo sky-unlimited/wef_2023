413e441e804257e33fae3892881c0304db019165
Alexandre Stanescot <alex@aerostan.com>
2023-12-23 17:05:22 +0100

    Capistrano staging secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,4 +40,4 @@ set :keep_releases, 3
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-append :linked_files, ".env.staging"
+append :linked_files, 'config/master.key'
