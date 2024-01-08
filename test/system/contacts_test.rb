eae43e2add0f5c0bf23bbc10a0b2eeb4e3c787d1
Alexandre Stanescot <alex@aerostan.com>
2024-01-08 21:55:24 +0100

    Contact system test: fix later

---
+++ test/system/contacts_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class ContactsTest < ApplicationSystemTestCase
+  #FIXME:Doesn't work...
+  #test "Correct form should save" do
+  #  visit new_contact_url
+  #  fill_in "Email", with: "alex@sky-unlimited.lu"
+  #  fill_in "Description", with: "This is a usefull description od my requirement"
+  #  check I18n.t("contact.form.accept_privacy_policy")
+  #  click_on "Submit"
+  #  assert_selector ".alerts", text: I18n.t("contact.notice.form_sent")
+  #end
+end
