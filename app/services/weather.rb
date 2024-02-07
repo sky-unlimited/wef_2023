16c1f5046622b8a3a9f02d792d03042d45eb58cd
Alexandre Stanescot <alex@aerostan.com>
2024-02-07 20:54:18 +0100

    Enhanced display for high wind conditions in forecasts

--- app/services/weather.rb
+++ app/services/weather.rb
@@ -11,18 +11,23 @@ module Weather
     weather_hash
   end
 
-  # Returns a hash following OpenWeather API 3.0 specs 
+  # Returns a hash following OpenWeather API 3.0 specs
   #   Provides a forecast for a defined latitude and longitude.
   #   Weather should be stored in cache.
   def read(lat, lon)
     # We generate a key depending on latitude and longitude
     key = "#{lat.round(3)}-#{lon.round(3)}"
 
+    # Retrieve the cache expiration duration
+    expires = WEF_CONFIG['weather_cache_expire_hour'].to_i
+
+    # Variables
+    url     = 'https://api.openweathermap.org/data/3.0/onecall'
+    app_id  = Rails.application.credentials[Rails.env.to_sym]
+                   .openweathermap.app_id
+
     # If key available in cache we retrieve, otherwise api call
-    Rails.cache.fetch(key, expires_in: 1.hours) do
-      url = 'https://api.openweathermap.org/data/3.0/onecall'
-      app_id = Rails.application.credentials[Rails.env.to_sym]
-                    .openweathermap.app_id
+    Rails.cache.fetch(key, expires_in: expires.hours) do
       JSON.parse(RestClient.get(url,
                                 { params:
                                          { lat:,
