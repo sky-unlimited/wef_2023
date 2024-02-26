dd7e572066a016d9110a56ac87e876fedb853529
Alexandre Stanescot <alex@aerostan.com>
2024-02-26 11:36:51 +0100

    Refactor airport view

--- app/helpers/airports_helper.rb
+++ app/helpers/airports_helper.rb
@@ -1,28 +1,46 @@
+# Helper to display info on airports view
 module AirportsHelper
   def extract_time_from_date(date)
-    return date.strftime('%H:%M')
+    date.strftime('%H:%M')
   end
 
   def display_fuel_provider_logo(airport)
     case airport.fuel_station.provider
     when FuelStation.providers.keys[1]
-      logo = FuelStation.inventory[:fuel_providers][:total_energies][:icon]
+      FuelStation.inventory[:fuel_providers][:total_energies][:icon]
     when FuelStation.providers.keys[2]
-      logo = FuelStation.inventory[:fuel_providers][:air_bp][:icon]
+      FuelStation.inventory[:fuel_providers][:air_bp][:icon]
     else
-      logo = FuelStation.inventory[:fuel_providers][:other][:icon]
+      FuelStation.inventory[:fuel_providers][:other][:icon]
     end
-    return logo
   end
 
+  # Should be deprecated
   def airport_fuel_types_icons(airport)
     fuel_type_icons = []
     unless airport.fuel_station.nil?
-      fuel_type_icons << FuelStation.inventory[:fuel_types][:fuel_avgas_100ll][:icon] if airport.fuel_station.fuel_avgas_100ll != "no"
-      fuel_type_icons << FuelStation.inventory[:fuel_types][:fuel_avgas_91ul][:icon] if airport.fuel_station.fuel_avgas_91ul != "no"
-      fuel_type_icons << FuelStation.inventory[:fuel_types][:fuel_mogas][:icon] if airport.fuel_station.fuel_mogas != "no"
-      fuel_type_icons << FuelStation.inventory[:fuel_types][:charging_station][:icon] if airport.fuel_station.charging_station != "no"
+      if airport.fuel_station.fuel_avgas_100ll != 'no'
+        fuel_type_icons << FuelStation.inventory[:fuel_types]
+        [:fuel_avgas_100ll][:icon]
+      end
+      if airport.fuel_station.fuel_avgas_91ul != 'no'
+        fuel_type_icons << FuelStation.inventory[:fuel_types]
+        [:fuel_avgas_91ul][:icon]
+      end
+      if airport.fuel_station.fuel_mogas != 'no'
+        fuel_type_icons << FuelStation.inventory[:fuel_types]
+        [:fuel_mogas][:icon]
+      end
+      if airport.fuel_station.charging_station != 'no'
+        fuel_type_icons << FuelStation.inventory[:fuel_types]
+        [:charging_station][:icon]
+      end
     end
-    return fuel_type_icons
+    fuel_type_icons
+  end
+
+  def display_poi_label(key)
+    label = PoiCatalogue.inventory[key]['label']
+    I18n.t("activerecord.attributes.trip_request.#{label}")
   end
 end
