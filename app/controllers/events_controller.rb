f9360a9fdbda31d47ad9db5dea278721ab1afabc
Christina Sugiono <christina.sugiono95@gmail.com>
2024-03-15 16:42:58 +0800

    feat: events#create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,19 @@ class EventsController < ApplicationController
     @event = Event.new
     @airports = Airport.all
   end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path, notice: 'Event was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :airport_id, :start_date, :end_date, :kind, :image_link, :url)
+  end
 end
