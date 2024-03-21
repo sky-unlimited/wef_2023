df9bb32b822349bf508379947fc269dd7776caad
Christina Sugiono <christina.sugiono95@gmail.com>
2024-03-21 17:06:20 +0800

    add auditlog

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,11 @@
 class FollowersController < ApplicationController
+  after_action :audit_log, only: [ :create ]
+
   def create
     following = User.find(params[:user_id])
-    if Follower.create(follower: current_user, following: following)
+    follow = Follower.create(follower: current_user, following: following)
+    if follow.save
+      AuditLog.create(action: 0, target_controller: 'followers', user_id: current_user.id, target_id: follow.id, ip_address: request.remote_ip)
       redirect_to request.path, notice: t('follower.created', username: following.username)
     end
   end
@@ -9,6 +13,7 @@ class FollowersController < ApplicationController
   def destroy
     follow = Follower.find(params[:id])
     if follow.destroy
+      AuditLog.find_by(target_controller: 'followers', target_id: follow.id).destroy
       redirect_to request.path, notice: t('follower.destroyed', username: follow.following.username)
     end
   end
