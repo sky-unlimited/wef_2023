2154cbec55ec3f1d17c0868182391641ca5a598d
Christina Sugiono <christina.sugiono95@gmail.com>
2024-03-21 14:59:04 +0800

    feat: pilots#update

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -7,4 +7,19 @@ class PilotsController < ApplicationController
     @pilot = Pilot.find(params[:id])
     @airports = Airport.all
   end
+
+  def update
+    @pilot = Pilot.find(params[:id])
+    if @pilot.update(pilot_params)
+      redirect_to pilot_path(@pilot), notice: 'Profile was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pilot_params
+    params.require(:pilot).permit(:airport_id, :airport_role, :aircraft_type, :bio)
+  end
 end
