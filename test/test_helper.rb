125500e11afe9e402785010cf9738a6f15543168
Christina Sugiono <christina.sugiono95@gmail.com>
2024-03-22 18:36:34 +0800

    improve test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,8 +13,6 @@ class ActiveSupport::TestCase
   include Devise::Test::IntegrationHelpers
   #include Warden::Test::Helpers
   #Warden.test_mode!
-
-  # Capybara::Screenshot.prune_strategy = { keep: 20 }
 end
 
 Capybara.save_path = Rails.root.join("tmp/capybara")
