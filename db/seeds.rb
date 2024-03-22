f24d6801404fec4559953530ef7635db78d8c687
Christina Sugiono <christina.sugiono95@gmail.com>
2024-03-22 13:59:17 +0800

    update comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,56 @@
 require 'csv'
 
-# ############################################
-# CLEANING
-# ############################################
-puts "👉 Cleaning databases..."
-AuditLog.destroy_all
-Preference.destroy_all
-User.destroy_all
-Runway.destroy_all
-FuelStation.destroy_all
-Event.destroy_all
-Airport.destroy_all
-Country.destroy_all
-Certificate.destroy_all
+if Rails.env.development? || Rails.env.staging?
+  # ############################################
+  # CLEANING
+  # ############################################
+  puts "👉 Cleaning databases..."
+  AuditLog.destroy_all
+  Preference.destroy_all
+  User.destroy_all
+  Runway.destroy_all
+  FuelStation.destroy_all
+  Event.destroy_all
+  Airport.destroy_all
+  Country.destroy_all
+  Certificate.destroy_all
 
-puts "-------------------------------"
-puts "All environments seeds"
-puts "-------------------------------"
+  puts "-------------------------------"
+  puts "All environments seeds"
+  puts "-------------------------------"
 
-# ############################################
-# IMPORT TASKS
-# ############################################
-Rake::Task['import:countries'].invoke
-Rake::Task['import:airports'].invoke
-Rake::Task['import:runways'].invoke
-Rake::Task['import:fuel_stations'].invoke
+  # ############################################
+  # IMPORT TASKS
+  # ############################################
+  Rake::Task['import:countries'].invoke
+  Rake::Task['import:airports'].invoke
+  Rake::Task['import:runways'].invoke
+  Rake::Task['import:fuel_stations'].invoke
 
-# ############################################
-# CERTIFICATES
-# ############################################
-# Create certificates
-ultra_light_pilot = Certificate.create!(name: "Ultra-light pilot")
-private_pilot = Certificate.create!(name: "Private pilot")
+  # ############################################
+  # CERTIFICATES
+  # ############################################
+  # Create certificates
+  ultra_light_pilot = Certificate.create!(name: "Ultra-light pilot")
+  private_pilot = Certificate.create!(name: "Private pilot")
 
-# ############################################
-# USERS
-# ############################################
-# Create an admin user
-user = User.new
-user.username = "alexstan57"
-user.email = "alex@sky-unlimited.lu"
-user.role = "admin"
-user.password = "Default2024"
-user.confirmed_at = Time.zone.now - 1.hour
-user.confirmation_sent_at = Time.zone.now - 2.hours
-user.save
-preference = Preference.find_by(user_id: user.id)
-preference.update(is_ultralight_pilot: true, is_private_pilot: false)
-puts "Admin user alex created"
-puts "Pilot pref alex created"
+  # ############################################
+  # USERS
+  # ############################################
+  # Create an admin user
+  user = User.new
+  user.username = "alexstan57"
+  user.email = "alex@sky-unlimited.lu"
+  user.role = "admin"
+  user.password = "Default2024"
+  user.confirmed_at = Time.zone.now - 1.hour
+  user.confirmation_sent_at = Time.zone.now - 2.hours
+  user.save
+  preference = Preference.find_by(user_id: user.id)
+  preference.update(is_ultralight_pilot: true, is_private_pilot: false)
+  puts "Admin user alex created"
+  puts "Pilot pref alex created"
+end
 
 # Create users
 if Rails.env.development? || Rails.env.staging?
@@ -92,3 +94,9 @@ if Rails.env.development? || Rails.env.staging?
     Event.create(title: "Event #{i + 1}", kind: kind, start_date: start_date, end_date: end_date, image_link: "https://source.unsplash.com/random/?#{airport.name}" , url: "https://google.com?q=#{airport.name}", airport: airport)
   end
 end
+
+# Create Certificates
+Certificate.create!(name: "Student")
+Certificate.create!(name: "Ultra-light/Microlight")
+Certificate.create!(name: "Private")
+Certificate.create!(name: "Commercial")
--- test/models/follower_test.rb
+++ test/models/follower_test.rb
@@ -1,7 +1,4 @@
 require "test_helper"
 
 class FollowerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
