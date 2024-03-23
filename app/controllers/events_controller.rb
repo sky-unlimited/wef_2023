5635a68eacc6d27031afe16d5a07489514a80f99
Alexandre Stanescot <alex@aerostan.com>
2024-03-23 16:33:14 +0100

    Translations path and style

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,4 @@
+# Manages the events around aviation
 class EventsController < ApplicationController
   before_action :check_admin, only: %i[new create edit update destroy]
 
@@ -13,7 +14,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      redirect_to events_path, notice: t('event.saved')
+      redirect_to events_path, notice: t('events.saved')
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +28,7 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-      redirect_to events_path, notice: t('event.updated')
+      redirect_to events_path, notice: t('events.updated')
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,18 +36,22 @@ class EventsController < ApplicationController
 
   def destroy
     @event = Event.find(params[:id])
-    if @event.destroy
-      redirect_to events_path, notice: t('event.deleted')
-    end
+    return unless @event.destroy
+
+    redirect_to events_path, notice: t('events.deleted')
   end
 
   private
 
   def check_admin
-    redirect_to root_path, alert: t('events.not_admin') unless current_user.admin?
+    return if current_user.admin?
+
+    redirect_to root_path,
+                alert: t('events.not_admin')
   end
 
   def event_params
-    params.require(:event).permit(:title, :airport_id, :start_date, :end_date, :kind, :image_link, :url)
+    params.require(:event).permit(:title, :airport_id, :start_date, :end_date,
+                                  :kind, :image_link, :url)
   end
 end
